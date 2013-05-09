c0a107da8e6649959f844ad0fe1efda7ccb92eb1
Nick Gray <nickjoegray@gmail.com>
2013-05-09 00:36:46 -0400

    apply style changes to notes#index & subjects#index, modify validations to notes.rb

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,11 +3,14 @@ class Note < ActiveRecord::Base
 
   validates :title, :presence => {:message => "should not be blank"}
   validates :searchable, :presence => {:message => "should not be blank"}
+  # validates :title, :presence => true, {:on => :save, :message => "should not be blank"
+  # validates :searchable, :presence => {:on => :save, :message => "should not be blank"}
 
   has_and_belongs_to_many :subjects
 
   searchable :auto_index => true do 
     text :title
+    
     text :searchable, :default_boost => 2
   end
 end
